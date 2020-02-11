4366836c794d589a186c865efbcb7ac129270fe3
MisoSoup477 <60865254+MisoSoup477@users.noreply.github.com>
2020-02-11 22:14:42 +0800

    有獎或挨罰,輸入數字和英文成績,2科都90分以上輸出有獎品,如果2科都不及格輸出要處罰,其中一科不及格輸出再加油

---
+++ 第二堂回家作業.py
@@ -0,0 +1,13 @@
+x=input('請輸入數學成績:')
+y=input('請輸入英文成績:')
+x=int(x)
+y=int(y)
+z=int(x+y)
+if z>=180:
+    print('有獎品!')
+else:
+    print('去罰站!')
+if z>=120:
+    print('再加油!')
+else:
+    print('去罰站!')
