1dc07fd97ee2b28808e3d0b2a3ec9660aa156f4a
MisoSoup477 <60865254+MisoSoup477@users.noreply.github.com>
2020-02-11 12:06:06 +0800

    計算有無及格

---
+++ 分數.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 11:07:40 2020
+
+@author: Administrator
+"""
+
+score=input('請輸入分數')
+score=int(score)
+if score>60:print('及格!')
+
+else:print(input("體重"))
