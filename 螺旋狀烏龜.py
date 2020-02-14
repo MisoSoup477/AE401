c4670512ad7926837687b59d6b32cff25602c429
MisoSoup477 <60865254+MisoSoup477@users.noreply.github.com>
2020-02-14 10:26:17 +0800

    用Turtle畫出螺旋狀圖

---
+++ 螺旋狀烏龜.py
@@ -0,0 +1,15 @@
+import turtle
+cngsugciugcergew=turtle.Turtle()
+bg=turtle.Screen()
+bg.bgcolor('black')
+cngsugciugcergew.color('red')
+cngsugciugcergew.shape('turtle')
+cngsugciugcergew.penup()
+for i in range(1,300):
+   cngsugciugcergew.stamp()
+   cngsugciugcergew.forward(10+i)
+   cngsugciugcergew.left(20)
+   
+   
+turtle.done()
+
