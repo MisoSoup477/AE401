2389a5b854e31a42192dc3ed99358608561de831
MisoSoup477 <60865254+MisoSoup477@users.noreply.github.com>
2020-03-21 10:08:09 +0800

    Update 401-02.py

--- 401-02.py
+++ 401-02.py
@@ -6,10 +6,13 @@ Created on Sat Mar  7 02:27:40 2020
 """
 
 from mcpi.minecraft import Minecraft
+#導入minecraft模組
 mc = Minecraft.create();
 
 while True:
+#while 迴圈
     x,y,z=mc.player.getTilePos()
     mc.postTo('You rae located on X:'+str(x)
                                +',Y:'+str(y)
                                +',Z:'+str(z))
+#說出You rae located on x,y,z
