1e5ad88525387f4a5c32550821a26c24f0dd8ffd
MisoSoup477 <60865254+MisoSoup477@users.noreply.github.com>
2020-03-21 10:10:28 +0800

    Update 401-02.py

--- 401-02.py
+++ 401-02.py
@@ -12,7 +12,8 @@ mc = Minecraft.create();
 while True:
 #while 迴圈
     x,y,z=mc.player.getTilePos()
+#玩家座標有3個值,所以開3個變數
     mc.postTo('You rae located on X:'+str(x)
                                +',Y:'+str(y)
                                +',Z:'+str(z))
-#說出You rae located on x,y,z
+#說出You rae located on x,y,z(x,y,z是玩家座標)
